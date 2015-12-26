9e989d42bed6ebf8b29962f5c1fe117d42aaae44
有村琢磨 <arimuratakuma@aritaku.local>
2015-12-26 18:19:46 +0900

    Initial Commit

---
+++ Mimamori/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mimamori
+//
+//  Created by 有村琢磨 on 2015/12/26.
+//  Copyright © 2015年 有村琢磨. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
